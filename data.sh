f63bf38f3fec041ab4bb972a14f23f8476871e2d
Vamsi66789 <141404338+Vamsi66789@users.noreply.github.com>
2023-09-08 16:33:45 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,6 @@
+sudo yum -y install git
+sudo git clone https://github.com/GOUSERABBANI44/flight-perdiction.git
 cd /home/ec2-user/workspace/m2slave-1
 sudo pip3 install -r requirements.txt
 python3 app.py
-sudo git clone https://github.com/GOUSERABBANI44/flight-perdiction.git 
+
